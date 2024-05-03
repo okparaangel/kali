bbdff588f0da3aad1d008d65606074adcdb17c78
angel <okparaangel@gmail.com>
2024-05-03 06:19:47 -0400

    Backup on Fri May  3 06:19:47 AM EDT 2024

--- GitBackup.sh
+++ GitBackup.sh
@@ -1,8 +1,12 @@
 !/bin/bash
+#You were giving a task to be placed on in a speacial folder on your system, and to ensure that anything in that folder is automatically commited using git. Also, the content is backed up to your github account. to ensure this, you had to learn about some basic git commands and a bash script that runs each time there is a change to anything in that folder. 
+
+username=$(whoami)
+current_date=$(date +%d)
 
 git init
 
-cd /Desktop/specialsh
+cd ./Desktop/specialsh
 
 git add .
 git commit -m "Backup on $(date)"
