a33ded2fb97da5041cb90d7b969f5bf3f527ecc0
angel <okparaangel@gmail.com>
2024-05-03 06:46:52 -0400

    Backup on Fri May  3 06:46:52 AM EDT 2024

--- GitBackup.sh
+++ GitBackup.sh
@@ -12,8 +12,9 @@ git add .
 git commit -m "Backup on $(date)"
 
 # Push changes to GitHub
-git push 
-git remote add origin https://github.com/okparaangel/kali.git
-git branch -M main
-git push -u origin main
-
+while true;do 
+	git push 
+	git remote add origin https://github.com/okparaangel/kali.git
+	git branch -M main
+	git push -u origin main
+done
